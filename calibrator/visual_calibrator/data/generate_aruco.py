08472495d21260e406704a3dbcd96f765d5a994d
jwansek <eddie.atten.ea29@gmail.com>
2023-11-28 15:16:39 +0000

    Updated aruco generator script

--- calibrator/visual_calibrator/data/generate_aruco.py
+++ calibrator/visual_calibrator/data/generate_aruco.py
@@ -1,8 +1,11 @@
 import cv2
 import numpy
+import sys
+
+SIZE = int(sys.argv[1])
 
 dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_250)
 for i in range(24, 28):
-    tag = numpy.zeros((500, 500, 1), dtype="uint8")
-    cv2.aruco.drawMarker(dictionary, i, 500, tag, 1)
-    cv2.imwrite("%d.png" % i, tag)
+    tag = numpy.zeros((SIZE, SIZE, 1), dtype="uint8")
+    cv2.aruco.drawMarker(dictionary, i, SIZE, tag, 1)
+    cv2.imwrite("%d_%ipx.png" % (i, SIZE), tag)
