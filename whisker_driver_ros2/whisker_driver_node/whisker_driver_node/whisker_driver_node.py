01aef9c9c99cac62e490006b9550051e4b18abc4
Liyou Zhou <liyou.zhou@outlook.com>
2024-01-10 01:30:57 +0000

    improve and fix driver

--- whisker_driver_ros2/whisker_driver_node/whisker_driver_node/whisker_driver_node.py
+++ whisker_driver_ros2/whisker_driver_node/whisker_driver_node/whisker_driver_node.py
@@ -13,6 +13,7 @@ from threading import Lock
 from copy import deepcopy
 from geometry_msgs.msg import PoseStamped
 import time
+import threading
 
 class WiskerDriverNode(Node):
     def __init__(self):
@@ -28,11 +29,15 @@ class WiskerDriverNode(Node):
         )
         self.declare_parameter(
             "classifier_rate_hz",
-            10,
+            10.0,
         )
+
+        serial_device = self.get_parameter("serial_device").get_parameter_value().string_value
+        baud_rate = self.get_parameter("baud_rate").get_parameter_value().integer_value
+        self.get_logger().info(f"Opening serial device: {serial_device} {baud_rate}")
         self.ser = serial.Serial(
-            self.get_parameter("serial_device"),
-            self.get_parameter("baud_rate"),
+            serial_device,
+            baud_rate,
             timeout=1,
         )
 
@@ -47,30 +52,49 @@ class WiskerDriverNode(Node):
         self.buffer = deque(maxlen=1000)
         self.buffer_lock = Lock()
 
-        self.classifier_timer = self.create_timer(1/self.get_parameter("classifier_rate_hz"), self.classifier_callback)
+        period = 1/self.get_parameter("classifier_rate_hz").get_parameter_value().double_value
+        self.classifier_timer = self.create_timer(period, self.classifier_callback)
         # self.classifier = Classifier()
 
+        self.serial_data_thread = threading.Thread(target=self.serial_data_thread_function)
+        self.serial_data_thread.start()
+        self.serial_data_thread_shutdown = False
 
-    def serial_data_thread(self):
-        while True:
+        self.publish_queue = deque(maxlen=1000)
+        self.publish_queue_lock = Lock()
+
+    def serial_data_thread_function(self):
+        self.get_logger().info("serial_data_thread_function")
+        while not self.serial_data_thread_shutdown:
             line = self.ser.readline().decode("utf-8")
+            self.get_logger().info("line" + line)
+
             stamp = time.time()
             match = self.pattern.search(line)
             if match is not None:
                 x, y, z = match.groups()
+                self.get_logger().info(f"{x} {y} {z}")
                 msg = MagneticField()
                 msg.header.frame_id = "magnetometer"
                 msg.header.stamp.sec = math.floor(stamp)
-                msg.header.stamp.nanosec = stamp % 1 * 1e9
+                msg.header.stamp.nanosec = round(stamp % 1 * 1e9)
                 msg.magnetic_field.x = int(x)
                 msg.magnetic_field.y = int(y)
                 msg.magnetic_field.z = int(z)
-                self.magnetometer_reading_publisher.publish(msg)
+                with self.publish_queue_lock:
+                    self.publish_queue.append(msg)
 
                 with self.buffer_lock:
                     self.buffer.append(msg)
 
     def classifier_callback(self):
+        self.get_logger().info(f"classifier_callback {len(self.publish_queue)}")
+
+        with self.publish_queue_lock:
+            while len(self.publish_queue) > 0:
+                msg = self.publish_queue.popleft()
+                self.magnetometer_reading_publisher.publish(msg)
+
         buffer_copy = None
         with self.buffer_lock:
             buffer_copy = deepcopy(self.buffer)
@@ -85,7 +109,8 @@ def main(args=None):
     rclpy.init(args=args)
     waypoints_mission_node = WiskerDriverNode()
     rclpy.spin(waypoints_mission_node)
-    waypoints_mission_node.close_serial()
+    waypoints_mission_node.serial_data_thread_shutdown = True
+    waypoints_mission_node.serial_data_thread.join()
     waypoints_mission_node.destroy_node()
     rclpy.shutdown()
 
